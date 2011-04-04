12dd64764c96365e4aaaf3bf15ecad54d0f05fcb
Jaymes Waters and Nick Karpenske <dev+jaym3s+randland@bendyworks.com>
2011-04-04 12:25:53 -0500

    Add initializer to load credentials.yml

--- lib/idkfa/railtie.rb
+++ lib/idkfa/railtie.rb
@@ -1,4 +1,24 @@
 module Idkfa
   class Railtie < Rails::Railtie
+    initializer "idkfa.load_credentials" do
+      credentials = Rails.root.join 'config/credentials.yml'
+      credentials_tmp = Rails.root.join 'config/credentials.yml.template'
+
+      # Copy or create credentials file
+      unless credentials.exist?
+        if credentials_tmp.exist?
+          FileUtils.cp credentials_tmp, credentials
+        else
+          credentials_tmp.mkpath
+          credentials.mkpath
+        end
+      end
+
+      # Load credentials based on environment
+      config = YAML.load_file(credentials)
+      config.fetch(Rails.env, {}).each do |key, value|
+        ENV[key.upcase] = value.to_s
+      end
+    end
   end
 end
